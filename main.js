a1cca5f30fadaf59f7a2e993439e8db1873bb7ce
HurairaKamran <hurairakamran2006@gmail.com>
2024-05-31 17:20:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//VARIABLES
+var vegetables = "cabbage";
+console.log(vegetables);
+vegetables = "carrot";
+console.log(vegetables);
+//CASES
+var myColour = "blue"; //camelcase
+var MyFruit = "mango"; //PascalCase
+var my_subject = "accounting"; //snake_case
+console.log(myColour);
+//OBJECT
+var detail = {
+    "name": "Huraira",
+    age: 18
+};
+console.log(detail.age);
+//DATATYPES
+var Fathername = "Huraira";
+var dob = 27022006;
+var maritalstatus = false;
+var website = "www.facebook.com";
+console.log(dob);
---
+++ main.ts
@@ -0,0 +1,32 @@
+                       //VARIABLES
+let vegetables="cabbage";
+console.log(vegetables);
+vegetables="carrot";
+console.log(vegetables);
+
+
+
+                       //CASES
+let myColour="blue";//camelcase
+let MyFruit="mango";//PascalCase
+let my_subject="accounting";//snake_case
+console.log(myColour)
+
+
+                        //OBJECT
+let detail={
+    "name":"Huraira",
+    age:18
+}
+console.log(detail.age)
+
+
+                       //DATATYPES
+let Fathername:string="Huraira";
+let dob:number=27022006;
+let maritalstatus:boolean=false;
+let website:any="www.facebook.com";
+console.log(dob)
+                             
+
+
